ba760b43fb707c0334ab502b45976ed84d8c967e
John Clow <johnwclow@hotmail.com>
2017-06-05 11:24:33 -0700

    Added the epsilon to the actor model

--- src/actor.py
+++ src/actor.py
@@ -3,6 +3,7 @@ from tensorflow.contrib import layers
 import numpy as np
 import logging
 import os
+import random
 
 logging.basicConfig(level=logging.INFO)
 train_dir = "./train_actor"
@@ -48,9 +49,9 @@ class Actor(object):
         :param seq_len: max number of questions to ask
         """
         self.num_cats = categories
-        self.moving_avg = 0
-        self.avg_counts = 0
-        self.action_applied = False
+        self._moving_avg = 0
+        self._avg_counts = 0
+        self._action_applied = False
         self.build_network(categories, cat_vec_len, hidden_sz, seq_len, dropout)
 
     def build_network(self, categories, cat_vec_len, hidden_sz, max_seq_len, dropout):
@@ -58,10 +59,9 @@ class Actor(object):
         self.cat_indicies = tf
         # Embeddings
         self.q = tf.placeholder(tf.int32, (None, max_seq_len))
-        self.cur_q_number = tf.placeholder(tf.int32, (None))  # the question for which we want an inference
         self.answ_correct = tf.placeholder(tf.float32, (None, max_seq_len))
         self.seq_len = tf.placeholder(tf.int32, (None))
-        with tf.VariableScope("Actor"):
+        with tf.variable_scope("Actor"):
             embed = tf.get_variable("embed", (categories, cat_vec_len), xav_init)
             q_embed = tf.nn.embedding_lookup(embed, self.q)
 
@@ -69,11 +69,10 @@ class Actor(object):
             cell = tf.nn.rnn_cell.LSTMCell(hidden_sz)
             cell = tf.nn.rnn_cell.DropoutWrapper(cell, output_keep_prob = 1.0 - dropout)
 
-            with tf.VariableScope("LSTM1"):
+            with tf.variable_scope("LSTM1"):
                 lstm1_out, lstm1_states = tf.nn.dynamic_rnn(cell=cell, inputs=lstm_in, sequence_length=self.seq_len,
                                                             dtype=tf.float32, swap_memory=True)
 
-            # lstm_index_i = lstm1_out[:, self.cur_q_number]
             hidden_1 = layers.fully_connected(lstm1_out, num_outputs=categories, scope="FC1")
             self.final_hid = layers.fully_connected(hidden_1, num_outputs=categories,
                                                     activation_fn=None, scope="FC2")
@@ -86,20 +85,22 @@ class Actor(object):
         opt = tf.train.AdamOptimizer(1e-3)
         self.train_op = opt.minimize(self.res * self.action_mask, self.action_gradient)
 
-    def get_next_action(self, session, question_hist, correct_hist, cur_q_num, seq_lens):
+    def get_next_action(self, session, question_hist, correct_hist, seq_lens, epsilon=0):
         """
-        :param question_hist: Questions given in the past (ndarray [Batch_sz])
-        :param correct_hist: Whether the question was answered correctly ([Batch_sz] NDarray)
-        :param step_nos: Step number to calculate each next step (ndarray [Batch_sz])
+        :param question_hist: Questions given in the past (ndarray [Batch_sz, N])
+        :param correct_hist: Whether the question was answered correctly ([Batch_sz, N] NDarray)
         :return: 
         """
-        self.action_applied = True
+        self._action_applied = True
+
+        if random.random() < epsilon:
+            self.next_action = np.random.choice(np.arange(self.num_cats), seq_lens.shape())
+            return self.next_action
 
         self.action_feed_dict = {
             self.q: question_hist,
             self.answ_correct: correct_hist,
-            self.cur_q_number: cur_q_num,
-            self.seq_len: seq_lens
+            self.seq_len: np.copy(seq_lens),
         }
         action_probs, = session.run([self.res], feed_dict=self.action_feed_dict)
         self.next_action = np.argmax(action_probs, axis=1)
@@ -108,19 +109,19 @@ class Actor(object):
     def apply_grad(self, session, action_perf):
 
         # first retrieve stored action
-        if not self.action_applied:
+        if not self._action_applied:
             raise Exception("You must call Actor.get_next_action before applying another training step")
-        self.action_applied = False
+        self._action_applied = False
 
         # get reference avg performance
-        self.moving_avg = (self.moving_avg * self.avg_counts + np.sum(action_perf)) / self.avg_counts + len(action_perf)
-        self.avg_counts += len(action_perf)
+        self._moving_avg = (self._moving_avg * self._avg_counts + np.sum(action_perf)) / self._avg_counts + len(action_perf)
+        self._avg_counts += len(action_perf)
 
         # now normalize to this average perf
         action_vec = np.zeros((len(action_perf), self.num_cats))
         action_mask = np.zeros_like(action_vec, dtype=np.bool)
 
-        for i, val in enumerate(action_perf - self.moving_avg):
+        for i, val in enumerate(action_perf - self._moving_avg):
             action_vec[i, self.next_action[i]] = val
             action_mask[i, self.next_action[i]] = True
 
@@ -129,7 +130,7 @@ class Actor(object):
             self.action_mask: action_mask
         })
 
-        session.run([self.train_op], feed_dict=action_perf)
+        loss = session.run([self.train_op], feed_dict=action_perf)
 
 
 

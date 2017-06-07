Added a moving average for each time step. Fixed the actor model

src/actor.py
@@ -16,14 +16,6 @@ def main():
 
     actor_model = Actor(100, 20)
 
-    """
-    with tf.Session() as sess:
-        initialize_model(sess, qa)
-
-        qa.train(sess, critic , train_dir)
-
-        qa.evaluate_answer(sess, critic, log=True)
-    """
 
 def initialize_model(session, model, train_dir=train_dir):
     ckpt = tf.train.get_checkpoint_state(train_dir)
@@ -49,8 +41,8 @@ class Actor(object):
         :param seq_len: max number of questions to ask
         """
         self.num_cats = categories
-        self._moving_avg = 0
-        self._avg_counts = 0
+        self._moving_avg = np.zeros((seq_len,))
+        self._avg_counts = np.zeros((seq_len,))
         self._action_applied = False
         self.build_network(categories, cat_vec_len, hidden_sz, seq_len, dropout)
 
@@ -59,15 +51,18 @@ class Actor(object):
         self.cat_indicies = tf
         # Embeddings
         self.q = tf.placeholder(tf.int32, (None, max_seq_len))
-        self.answ_correct = tf.placeholder(tf.float32, (None, max_seq_len))
+        self.answ_correct = tf.placeholder(tf.int32, (None, max_seq_len))
         self.seq_len = tf.placeholder(tf.int32, (None))
         with tf.variable_scope("Actor"):
-            embed = tf.get_variable("embed", (categories, cat_vec_len), dtype=tf.float32
+            embed = tf.get_variable("embed", (categories * 2, cat_vec_len), dtype=tf.float32
                                     , initializer=xav_init)
-            q_embed = tf.nn.embedding_lookup(embed, self.q)
+            # embed = tf.get_variable("embed",  dtype=tf.float32
+            #                        , initializer=np.identity(categories * 2, dtype=np.float32))
+            q_embed = tf.nn.embedding_lookup(embed, self.q + categories * self.answ_correct)
 
-            answ_correct_3d = tf.reshape(self.answ_correct, (-1, max_seq_len, 1))
-            lstm_in = tf.concat([q_embed, answ_correct_3d], axis=2)
+            #answ_correct_3d = tf.reshape(self.answ_correct, (-1, max_seq_len, 1))
+            # lstm_in = tf.concat([q_embed, answ_correct_3d], axis=2)
+            lstm_in = q_embed
             cell = tf.contrib.rnn.LSTMCell(hidden_sz)
             cell = tf.contrib.rnn.DropoutWrapper(cell, output_keep_prob=1.0 - dropout)
 
@@ -75,7 +70,7 @@ class Actor(object):
                 lstm1_out, lstm1_state = tf.nn.dynamic_rnn(cell=cell, inputs=lstm_in, sequence_length=self.seq_len,
                                                             dtype=tf.float32, swap_memory=True)
 
-            hidden_1 = layers.fully_connected(lstm1_state.h, num_outputs=categories, scope="FC1")
+            hidden_1 = layers.fully_connected(lstm1_state.h, num_outputs=categories * 4, scope="FC1")
             self.final_hid = layers.fully_connected(hidden_1, num_outputs=categories,
                                                     activation_fn=None, scope="FC2")
             self.res = tf.nn.softmax(self.final_hid)
@@ -85,9 +80,10 @@ class Actor(object):
         self.action_mask = tf.placeholder(tf.bool, [None, categories])
         self.action_gradient = tf.placeholder(tf.float32, [None, categories])
         # the minimize function for the adam loss has a "grad_loss" param that is useful
-        opt = tf.train.AdamOptimizer(1e-2)
+        opt = tf.train.AdamOptimizer(1e-3)
         mask_res = self.res * tf.cast(self.action_mask, dtype=tf.float32)
-        self.train_op = opt.minimize(mask_res, grad_loss=-self.action_gradient)
+        # self.train_op = opt.minimize(mask_res, grad_loss=-self.action_gradient / self.res)
+        self.train_op = opt.minimize(tf.reduce_sum(-tf.log(self.res) * self.action_gradient))
 
     def get_next_action(self, session, question_hist, correct_hist, seq_lens,
                         collect_action_probs=False, epsilon=0.05):
@@ -104,14 +100,16 @@ class Actor(object):
             self.seq_len: np.copy(seq_lens),
         }
 
-        if random.random() < epsilon:
-            self.next_action = np.random.choice(np.arange(self.num_cats), seq_lens.shape)
-            return self.next_action
+        # if random.random() < epsilon:
+        #     self.next_action = np.random.choice(np.arange(self.num_cats), seq_lens.shape)
+        #     return self.next_action
 
         action_probs, = session.run([self.res], feed_dict=self.action_feed_dict)
 
+        # prep work for other partsj
         if collect_action_probs:
             self.action_probs.append(action_probs)
+        self.seq_lens = seq_lens
 
         # self.next_action = np.argmax(action_probs, axis=1)
         next_a = []
@@ -128,14 +126,17 @@ class Actor(object):
         self._action_applied = False
 
         # get reference avg performance
-        self._moving_avg = (self._moving_avg * self._avg_counts + np.sum(action_perf)) / \
-                           (self._avg_counts + len(action_perf))
-        self._avg_counts += len(action_perf)
+        s = self.seq_lens[0] - 1
+        assert np.all(s + 1 == self.seq_lens)
+
+        self._moving_avg[s] = (self._moving_avg[s] * self._avg_counts[s] + np.sum(action_perf)) / \
+                           (self._avg_counts[s] + len(action_perf))
+        self._avg_counts[s] += len(action_perf)
 
         # now normalize to this average perf
         action_vec = np.zeros((len(action_perf), self.num_cats), dtype=np.float32)
         action_mask = np.zeros_like(action_vec, dtype=np.bool)
-        action_delta = action_perf - self._moving_avg
+        action_delta = action_perf - self._moving_avg[s]
 
         for i, val in enumerate(action_delta):
             action_vec[i, self.next_action[i]] = val

src/test_actor.py
@@ -13,27 +13,32 @@ batch_size = 5
 seq_len = 8
 
 
+# debug action variables
+
 # Now check out what the model infers:
 
 
 def run_model(model, session, test=False):
-    critic_scores = np.zeros((batch_size, 2))
+    critic_scores = np.zeros((batch_size, 2))  # how much we actually learn
     q_hist = np.zeros((batch_size, seq_len))
     correct_hist = np.zeros((batch_size, seq_len), dtype=np.bool)
-    seq_lens = np.ones((batch_size,))
+    seq_lens = np.ones((batch_size,), dtype=np.int32)
     if test: model.action_probs = []
     for j in range(seq_len):
-        extra_args = dict(collect_action_probs=True, epsilon=0) if test else {}
+        extra_args = dict(collect_action_probs=True) if test else {}
 
-        actions = model.get_next_action(session, q_hist, correct_hist, seq_lens, **extra_args)
+        actions = model.get_next_action(session, q_hist, correct_hist, seq_lens, epsilon=0, **extra_args)
         learning = np.zeros((batch_size,))
         for an, action in enumerate(actions):
             if critic_scores[an, action] < 1:
                 if action == 0:
                     learning[an] = 0.1
                 else:
-                    learning[an] = 0.2
+                    # learning[an] = 0.4
+                    learning[an] = 0.25 - 0.02 * j  # more useful to learn early
                 critic_scores[an, action] += learning[an]
+            else:
+                assert learning[an] == 0
             correct_hist[an, j] = (random.random() < critic_scores[an, action])
 
         q_hist[:, j] = actions
@@ -50,16 +55,18 @@ def run_model(model, session, test=False):
         print(array_probs[:, :, 1].mean(axis=1))  # in this case the batch is on axis 1....
 
 
-        print("All Probabilities:")
-        print(model.action_probs)
+        # print("All Probabilities:")
+        # print(model.action_probs)
 
 
 def test_system():
-    model = actor.Actor(categories=2, cat_vec_len=10, seq_len=seq_len)
+    model = actor.Actor(categories=2, cat_vec_len=seq_len, seq_len=seq_len)
 
     with tf.Session() as session:
         session.run(tf.global_variables_initializer())
         for i in range(300):
+            if i % 20 == 0:
+                run_model(model, session, test=True)  # see behavior over time.
             run_model(model, session)
 
         run_model(model, session, test=True)